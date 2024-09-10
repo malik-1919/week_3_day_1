45b278359af81c705aeb0e294f1fdb02c377b493
Hreish <mahreish@cps.k12.il.us>
2024-09-10 09:34:45 -0500

    f strings and concatenation

---
+++ week_3_day_1.py
@@ -0,0 +1,193 @@
+# # Review
+ # create variables for the following :
+ # 1. age
+age= 17 #integer variable 
+ # 2. name
+name= "Malik" #string variable 
+ # 3. song
+song=" Pride" #string variable 
+ # 4. food
+food= "pizza" #string variable 
+ # 5. number
+number= 30 #integer variable 
+ 
+
+ # #now include the variables you just made print in the following...
+# Once upon a time, there was a [age] old coder named [name].
+#concatenation--- + around your variables
+print("once upon a time there was a" +str (age) + "year old coder named" + name + ",")
+print("there was a number" +str (number)+"as well")
+#put the age and number in a new sentence 
+print("The age" +str(age)+ "and the number" +str(number)+ "are numbers") 
+# [name] liked to hum the song [song] while coding. It was so annoying that their teammates would throw [food] until [name] would stop singing.
+date_of_birth=2021
+Number2=123
+number3=123.456
+number4=123.33
+number5=4555
+#create a sentence of all this above
+print("date of birth is"+ str(date_of_birth)+"and they are somehow"+str(Number2) +"," +str(number3) +","+str(number4)+","+str (number5)+"years old")
+print(f"date of birth of is {date_of_birth} and is {number3}")
+# Still, [name] was the best coder on the team and could write [number] lines of code every day. Maybe [song] was [name]’s secret power?
+##########################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##########################################################################################
+# The names you use when creating these labels need to follow a few rules:
+# 1. Names can not start with a number.
+# 2. There can be no spaces in the name, use _ instead.
+# 3. Can't use any of these symbols :'",<>/?|\()!@#$%^&*~-+
+# 4. It's considered best practice (PEP8) that names are lowercase.
+# 5. Avoid using the characters 'l' (lowercase letter el), 'O' (uppercase letter oh),
+#    or 'I' (uppercase letter eye) as single character variable names.
+# 6. Avoid using words that have special meaning in Python like "list" and "str"
+
+
+# Here are some exercises to practice the rules:
+
+
+# Correcting Invalid Names: Below are some invalid names. Correct them according to the rules:
+
+
+# 1st_name
+# last name
+# email@address
+# percent%
+# variable#name
+# O
+# list
+# Creating Valid Names: Create valid names for the following descriptions:
+
+
+# The first name of a person
+# The last name of a person
+# The email address of a person
+# The percentage of marks obtained by a student
+# A variable to store the number of items in a shopping cart
+
+
+
+
+# Identify Valid and Invalid Names: Identify which of the following names are valid or invalid according to the rules:
+
+
+# first_name
+# lastName
+# email_address
+# percentage
+# variable_name
+# 1_variable
+# email@address
+# percentage%
+# i
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+############################################################################################
+
+
+# # **Working with** **numbers** **bold text**
+# We'll learn about the following topics:
+# 1. Types of Numbers in Python
+# 2. Basic Arithmetic
+# 3. Differences between classic division and floor division
+
+
+# Python has various "types" of numbers (numeric literals).
+# 1. We'll mainly focus on integers and floating point numbers.
+# Integers are just whole numbers, positive or negative. For example: 2 and -2 are examples of integers.
+# 2. Floating point numbers in Python are notable because they have a decimal point in them, or use an exponential (e) to define the number. For example 2.0 and -2.1 are examples of floating point numbers. 4E2 (4 times 10 to the power of 2) is also an example of a floating point number in Python.
+
+
+##########################################################################################
+# #addition
+# #multiplication
+# #division
+# #modulo
+# #powers
+# #get the max and min of a number
+# #round a number
+# # absolute value
+# # order of operations
+# #to do more you need to import special math libraries from python
+# #from math import *    
+# #this goes out and grabs some different math functions we can use
+# #floor method
+# #ceil method
+# #sqrt method
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##########################################################################################
+# So what have we learned? We learned some of the basics of numbers in Python. We also learned how to do arithmetic and use Python as a basic calculator. We then wrapped it up with learning about Variable Assignment in Python.
+# # **Getting Input from users**
+# #how do we get input from users?
+# input("what is your name?")
+# # basic math calculator
+# #ask the user for 2 numbers
+# # print out a statement where you:
+# # add them together
+# #multiply
+# # find the max number
+# # find the remainder of the numbers
+# #round one number
+
+
+
+
+
+
+
+
+##########################################################################################
+# # mad libs game
+# print("Roses are {color}")
+# print("{plural noun} are blue")
+# print("I love {celebrity}")
+# # On to codehs.com
+
+
+
+
+
+
